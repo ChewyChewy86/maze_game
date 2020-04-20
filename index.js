99113e4eed1d011f73268bdec7b4234c2fe334b2
unknown <trainguitar@gmail.com>
2020-04-20 20:08:26 +0100

    Add maze walls and boundry logic

--- index.js
+++ index.js
@@ -8,9 +8,10 @@ const {
   Mouse,
 } = Matter;
 
-const cells = 3;
+const cells = 10;
 const width = 600;
 const height = 600;
+const unitLength = width / cells;
 
 const engine = Engine.create();
 const { world } = engine;
@@ -27,19 +28,12 @@ const render = Render.create({
 Render.run(render);
 Runner.run(Runner.create(), engine);
 
-World.add(
-  world,
-  MouseConstraint.create(engine, {
-    mouse: Mouse.create(render.canvas),
-  })
-);
-
 // Walls
 const walls = [
-  Bodies.rectangle(width / 2, 0, width, 40, { isStatic: true }),
-  Bodies.rectangle(width / 2, height, width, 40, { isStatic: true }),
-  Bodies.rectangle(0, height / 2, 40, height, { isStatic: true }),
-  Bodies.rectangle(width, width / 2, 40, width, { isStatic: true }),
+  Bodies.rectangle(width / 2, 0, width, 2, { isStatic: true }),
+  Bodies.rectangle(width / 2, height, width, 2, { isStatic: true }),
+  Bodies.rectangle(0, height / 2, 2, height, { isStatic: true }),
+  Bodies.rectangle(width, width / 2, 2, width, { isStatic: true }),
 ];
 World.add(world, walls);
 
@@ -73,7 +67,7 @@ const verticals = Array(cells)
 // Horizontal
 const horizontals = Array(cells - 1)
   .fill(null)
-  .map(() => Array(3).fill(false));
+  .map(() => Array(cells).fill(false));
 
 const startRow = Math.floor(Math.random() * cells);
 const startColumn = Math.floor(Math.random() * cells);
@@ -88,15 +82,15 @@ const cellRecursion = (row, column) => {
 
   // Asseble randomly-ordered list of neighbors
   const neighbors = shuffle([
-    [row - 1, column, 'up'],
-    [row, column + 1, 'right'] ,
-    [row + 1, column, 'down'],
-    [row, column + 1, 'left'],
+    [row - 1, column, "up"],
+    [row, column + 1, "right"],
+    [row + 1, column, "down"],
+    [row, column - 1, "left"],
   ]);
-  console.log(neighbors);
+  // console.log(neighbors);
   // For each neighbor...
   for (let neighbor of neighbors) {
-    const [nextRow, nextColumn] = neighbor;
+    const [nextRow, nextColumn, direction] = neighbor;
 
     // See if that neighbor is out of bounds
     if (
@@ -108,14 +102,97 @@ const cellRecursion = (row, column) => {
       continue;
     }
     // If we have visted that neighbor, continue to next neighbor
-    if (grid[nextrow][nextColumn]) {
+    if (grid[nextRow][nextColumn]) {
       continue;
     }
     // Remove a wall from either horizontals or verticals
+    if (direction === "left") {
+      verticals[row][column - 1] = true;
+    } else if (direction === "right") {
+      verticals[row][column] = true;
+    } else if (direction === "up") {
+      horizontals[row - 1][column] = true;
+    } else if (direction === "down") {
+      horizontals[row][column] = true;
+    }
+
+    cellRecursion(nextRow, nextColumn);
   }
 
   // Visit that next cell
 };
 
-cellRecursion(1, 1);
-// console.log(grid);
+cellRecursion(startRow, startColumn);
+
+horizontals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) {
+      return;
+    }
+
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength / 2,
+      rowIndex * unitLength + unitLength,
+      unitLength,
+      5,
+      {
+        isStatic: true,
+      }
+    );
+    World.add(world, wall);
+  });
+});
+
+verticals.forEach((row, rowIndex) => {
+  row.forEach((open, columnIndex) => {
+    if (open) {
+      return;
+    }
+
+    const wall = Bodies.rectangle(
+      columnIndex * unitLength + unitLength,
+      rowIndex * unitLength + unitLength / 2,
+      5,
+      unitLength,
+      {
+        isStatic: true,
+      }
+    );
+    World.add(world, wall);
+  });
+});
+
+// Goal
+
+const goal = Bodies.rectangle(
+  width - unitLength / 2,
+  height - unitLength / 2,
+  unitLength * 0.7,
+  unitLength * 0.7,
+  {
+    isStatic: true,
+  }
+);
+World.add(world, goal);
+
+// Ball
+
+const ball = Bodies.circle(unitLength / 2, unitLength / 2, unitLength / 4, {
+  isStatic: false,
+});
+World.add(world, ball);
+
+document.addEventListener("keydown", (event) => {
+  if ((event.keyCode === 38)) {
+    console.log("up");
+  }
+  if ((event.keyCode === 40)) {
+    console.log("down");
+  }
+  if ((event.keyCode === 39)) {
+    console.log("right");
+  }
+  if ((event.keyCode === 37)) {
+    console.log("left");
+  }
+});
